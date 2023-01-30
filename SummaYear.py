2bca6f56bab8b95def632cb4679d903b5f94467f
N1chBoy <isip_i.i.efremov@mpt.ru>
2023-01-30 10:33:33 +0300

    Календарь

--- SummaYear.py
+++ SummaYear.py
@@ -11,7 +11,7 @@ def program(year):
     october = 31
     november = 30
     december = 31
-    if (year % 4 == 0):
+    if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
         february = 29
         print("Год - високосный")
     else: 
@@ -31,21 +31,6 @@ def sumMonth(month):
     for i in range(month):
         if (month < 10):
             sum += i
-        else: divmod(sum)
+        else: sum += (i // 10) + (i % 10)
     return sum
 print(program(year))
-
-#if (calendar.isleap(year) == True)
-# year1 = array([cal.datetime.date.day])
-# divmod(year1)
-# sum(year1)
-# if (year % 4 == 0):
-#     february  = int(1+2+3+4+5+6+7+8+9+1+0+1+1+1+2+1+3+1+4+1+5+1+6+1+7+1+8+1+9+2+0+2+1+2+2+2+3+2+4+2+5+2+6+2+7+2+8+2+9)
-#     summa = january + february + march + april + may + june + july + august + september + october + november + december
-#     
-#     print(summa)
-# else:
-#     february  = int(1+2+3+4+5+6+7+8+9+1+0+1+1+1+2+1+3+1+4+1+5+1+6+1+7+1+8+1+9+2+0+2+1+2+2+2+3+2+4+2+5+2+6+2+7+2+8)
-#     summa = january + february + march + april + may + june + july + august + september + october + november + december
-#     
-#     print(summa)
